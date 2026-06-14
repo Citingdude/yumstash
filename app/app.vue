a0765bd7e4f3984ded559daf3e3b5e85289f1a12
glenn <glenn.reumers@appwise.be>
2026-06-14 11:34:29 +0200

    feat(app): add command palette

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import AppCommandPalette from '~/components/command/AppCommandPalette.vue'
+</script>
+
 <template>
   <UApp>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+
+    <AppCommandPalette />
   </UApp>
 </template>
---
+++ app/components/command/AppCommandPalette.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { CommandPaletteGroup } from '@nuxt/ui'
+
+const open = ref(false)
+
+defineShortcuts({
+  meta_k: () => {
+    open.value = !open.value
+  },
+})
+
+const groups: CommandPaletteGroup[] = [
+  {
+    id: 'navigation',
+    label: 'Navigation',
+    items: [
+      {
+        label: 'Dashboard',
+        icon: 'i-lucide-layout-dashboard',
+        to: '/',
+        onSelect: () => {
+          open.value = false
+        },
+      },
+      {
+        label: 'Settings',
+        icon: 'i-lucide-settings',
+        to: '/settings',
+        onSelect: () => {
+          open.value = false
+        },
+      },
+    ],
+  },
+]
+</script>
+
+<template>
+  <UModal v-model:open="open">
+    <template #content>
+      <UCommandPalette
+        close
+        :groups="groups"
+        placeholder="Search..."
+        class="h-80"
+        @update:open="open = $event"
+      />
+    </template>
+  </UModal>
+</template>
